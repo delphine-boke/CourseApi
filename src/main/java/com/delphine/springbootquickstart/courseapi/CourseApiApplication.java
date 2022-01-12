37d25c775796b48ce0cc071f387f9dcea354f750
delphine nyaboke <delphine@sendyit.com>
2022-01-12 07:15:41 +0300

    Hello controller class

--- src/main/java/com/delphine/springbootquickstart/courseapi/CourseApiApplication.java
+++ src/main/java/com/delphine/springbootquickstart/courseapi/CourseApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CourseApiApplication.class, args);
 	}
 
